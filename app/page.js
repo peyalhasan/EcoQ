3b49a9ba7255e308d2d810d9855edb678dbe8829
Peyal Hasan <peyalhasan143@gmail.com>
2026-02-03 16:52:09 +0600

    .env

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import LocationDetactor from "@/components/LocationDetactor";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="!z-50 w-full">
       <div className="container">
+        <Suspense fallback={<div>Loading location Detector...</div>}>
+        </Suspense>
         <LocationDetactor />
       </div>
     </main>
